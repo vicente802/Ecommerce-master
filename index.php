b559e0f115b0089b1aa4db32e4cfbae63e069bdf
JM TIANA <89765111+MakieMac@users.noreply.github.com>
2022-02-07 23:39:49 +0800

    save

--- index.php
+++ index.php
@@ -37,8 +37,6 @@ if(isset($_SESSION["uid"])){
 				<li><a href="index.php"><span class="glyphicon glyphicon-home"></span>Home</a></li>
 				<li><a href="index.php"><span class="glyphicon glyphicon-modal-window"></span>Product</a></li>
 				<li><a href="services.php"><span class="glyphicon glyphicon-modal-window"></span>Services</a></li>
-				
- 				<li><a href="services.php"><span class="glyphicon glyphicon-modal-window"></span>Services</a></li>
 			</ul>
 			<form class="navbar-form navbar-left">
 		        <div class="form-group">
