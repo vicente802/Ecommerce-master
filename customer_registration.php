ff080c2c562bf0883ebc4c7c0cd8f578faf3bd0a
JM TIANA <89765111+MakieMac@users.noreply.github.com>
2022-02-07 22:51:08 +0800

    save

--- customer_registration.php
+++ customer_registration.php
@@ -7,7 +7,7 @@ if (isset($_GET["register"])) {
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>Khan Store</title>
+		<title>Hardcore Motorshop</title>
 		<link rel="stylesheet" href="css/bootstrap.min.css"/>
 		<script src="js/jquery2.js"></script>
 		<script src="js/bootstrap.min.js"></script>
@@ -21,7 +21,7 @@ if (isset($_GET["register"])) {
 	<div class="navbar navbar-inverse navbar-fixed-top">
 		<div class="container-fluid">	
 			<div class="navbar-header">
-				<a href="#" class="navbar-brand">Khan Store</a>
+				<a href="#" class="navbar-brand">Hardcore Motorshop</a>
 			</div>
 			<ul class="nav navbar-nav">
 				<li><a href="index.php"><span class="glyphicon glyphicon-home"></span>Home</a></li>
