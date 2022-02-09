b5f7c2de0078950efb5772a02dba88f541b9210b
vicente802 <69038580+vicente802@users.noreply.github.com>
2022-02-10 00:33:59 +0800

    save

--- main.js
+++ main.js
@@ -143,7 +143,7 @@ $(document).ready(function() {
                 success: function(data) {
                     $(".overlay").hide();
                     if (data == "register_success") {
-                        window.location.href = "cart.php";
+                        window.location.href = "index.php";
                     } else {
                         $("#signup_msg").html(data);
                     }
