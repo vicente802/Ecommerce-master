cb33a6f805444769be100656d65bf3a4ccb4c27d
yangg1115 <98650888+yangg1115@users.noreply.github.com>
2022-03-06 02:14:21 +0800

    save

--- sms/verification.php
+++ sms/verification.php
@@ -26,22 +26,85 @@ if(isset($_POST['submit'])){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
+<meta charset="UTF-8">
+		<title>Hardcore Motorshop</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+		<link rel="stylesheet" href="../css/bootstrap.min.css"/>
+		<script src="../js/jquery2.js"></script>
+		<script src="../js/bootstrap.min.js"></script>
+		<script src="../main.js"></script>
+	
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
 	<title>Autofocus Field</title>
 	<link rel="stylesheet" type="text/css" href="verification.css">
+
 </head>
 <body>
-	<div class="container">
-		<h1>ENTER OTP</h1>
-		<div class="userInput">
-			<input type="text" id='ist' maxlength="1" onkeyup="clickEvent(this,'sec')">
-			<input type="text" id="sec" maxlength="1" onkeyup="clickEvent(this,'third')">
-			<input type="text" id="third" maxlength="1" onkeyup="clickEvent(this,'fourth')">
-			<input type="text" id="fourth" maxlength="1" onkeyup="clickEvent(this,'fifth')">
-			<input type="text" id="fifth" maxlength="1">
-		</div>
-		<button>CONFIRM</button>
-	</div>
+<div class="wait overlay">
+	<div class="loader"></div>
+</div>
+	<div class="navbar navbar-inverse navbar-expand-lg navbar-fixed-top">
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false">
+					<span class="sr-only"> navigation toggle</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="#" class="navbar-brand" style="margin-left: 5px;color:white;">Hardcore Motorshop</a>
+			</div>
+		<div class="collapse navbar-collapse" id="collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="index2.php"><span class="glyphicon glyphicon-home">&nbsp;</span>Home</a></li>
+				<li><a href="profile.php"><span class="glyphicon glyphicon-modal-window">&nbsp;</span>Product</a></li>
+				<li><a href="services.php"><span class="glyphicon glyphicon-globe">&nbsp;</span>Services</a></li>
+				<li><a href="contactus.php"><span class="glyphicon glyphicon-earphone">&nbsp;</span>Contact Us</a></li>
+				
+			</ul>
+</div>
+</div>
+</div>
+</div>
+
+<div>
+<div class="container d-flex justify-content-center align-items-center">
+
+<div class="card text-center">
+
+   <div class="card-header p-5">
+       <img src="mobile.png">
+       <h5 class="mb-2">OTP VERIFICATION</h5>
+       <div>
+           <small>code has been send to ******1258</small>
+       </div>
+   </div>
+
+   <div class="input-container d-flex flex-row justify-content-center mt-2">
+       <input type="text" class="m-1 text-center form-control rounded" maxlength="1">
+       <input type="text" class="m-1 text-center form-control rounded" maxlength="1">
+       <input type="text" class="m-1 text-center form-control rounded" maxlength="1">
+       <input type="text" class="m-1 text-center form-control rounded" maxlength="1">
+       <input type="text" class="m-1 text-center form-control rounded" maxlength="1">
+   </div>
+
+<div>
+    <small>
+        didn't get the otp
+       <a href="#" class="text-decoration-none">Resend</a>
+    </small>
+</div>
+
+<div class="mt-3 mb-5">
+    <button class="btn btn-success px-4 verify-btn">verify</button>
+</div>
+
+</div>
+
+</div>
+
 </body>
 </html>
