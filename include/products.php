503e3ca50061883a9c113dc92523b11a7ce60f9f
vicente802 <69038580+vicente802@users.noreply.github.com>
2022-03-06 00:45:34 +0800

    save

--- include/products.php
+++ include/products.php
@@ -61,6 +61,7 @@ if (!$con) {
 									<img style='position:relative; background:white; margin-top:-100px; width:80px; margin-left:-130px; z-index:1;'  src='imgs/best2.jpg'><img src='product_images/$pro_image' style='width:160px; height:160px;'/>
 								</div>
 								<div class='panel-heading'>".CURRENCY." $pro_price.00
+                                <input type='hidden' name='prod_id' value=$pro_id>	
 									<button pid='$pro_id' style='float:right;' id='product' class='btn btn-danger btn-xs'>AddToCart</button>
 									<button style='float:right;' id='$pro_id' class='btn btn-primary btn-xs details-btn' data-toggle='modal' data-target=''>details</button>
 									</div>
